62de76742eebc5fda5968aee69617e543feb176b
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-02 13:58:08 +0300

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,12 +6,16 @@ Task 0: Regex-int
 import re
 from typing import List
 
-
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
 
-def filter_datum(fields: List[str], redaction:str, message:str, separator:str) -> str:
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
     extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(map(re.escape, fields), re.escape(separator)), replace(redaction), message)
+    return re.sub(extract(
+        map(re.escape, fields), re.escape(separator)
+        ), replace(redaction), message)
