5108984df2013c41d28362e3fc9387caae4db0c3
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-02 14:15:41 +0300

    Logging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -41,6 +41,7 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """Format the log
         """
-        record.msg = filter_datum(
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(
             self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+        return txt
