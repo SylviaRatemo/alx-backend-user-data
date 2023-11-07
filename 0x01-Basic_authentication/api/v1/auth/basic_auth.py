7e5cf9e9cfbf4fbefd01312854836aa5e8f1a003
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-07 22:33:55 +0300

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 class to perform basic authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[len('Basic '):].strip()
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """Decode BAse4 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode_value = base64.b64decode(base64_authorization_header)
+            return decode_value.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
