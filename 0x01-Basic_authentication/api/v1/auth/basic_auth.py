59f5a114c5672aa304195109ba3cc0d1558042b8
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-07 21:47:52 +0300

    Task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+class to perform basic authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """class to handle basic authentication
+    """
+    
