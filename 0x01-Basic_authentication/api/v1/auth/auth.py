5fc66e5bc8ee36294bfdf50a471e9958803b354f
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-07 15:34:49 +0300

    Dummy auth implementations

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+class to manage the API authentication.
+"""
+from flask import request
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Dummy implementation for require_auth.
+        Returns False for now.
+
+        Args:
+            path (str): The path to check for authentication.
+            excluded_paths (List[str]): List of paths to exclude from authentication.
+
+        Returns:
+            bool: Always returns False.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Dummy implementation for authorization_header.
+        Returns None for now.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            str: Always returns None.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Dummy implementation for current_user.
+        Returns None for now.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            TypeVar('User'): Always returns None.
+        """
+        return None
