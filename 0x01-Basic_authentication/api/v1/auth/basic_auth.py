cf678bcdb0f92996b4330e2c380f1f07f6ddfa4f
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-07 22:17:44 +0300

    Task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,6 +12,8 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
             self, authorization_header: str
             ) -> str:
+        """Extract the Base64 authorization header
+        """
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
