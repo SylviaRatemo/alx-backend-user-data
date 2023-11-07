9caf9381542ca350297684f0a684cb532b653035
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-07 22:01:42 +0300

    Task 6

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,6 +8,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """class to handle basic authentication
     """
+    
     def extract_base64_authorization_header(
             self, authorization_header: str
             ) -> str:
