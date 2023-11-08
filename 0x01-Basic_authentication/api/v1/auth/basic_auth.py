da3ec50cbf21a440d30bb21f79ba0c3463f3c5bd
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-08 08:20:31 +0300

    Task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ class to perform basic authentication
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -52,3 +54,23 @@ class BasicAuth(Auth):
         else:
             email, pwd = decoded_base64_authorization_header.split(":")
             return email, pwd
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str
+            ) -> TypeVar('User'):
+        """Check if user exists in database
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        user_list =  User.search({'email': user_email})
+
+        if not user_list:
+            return None
+
+        for user in user_list:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
