9bdb1fb93d1d593b0897315ffca4ff8cd8d0e430
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-08 07:05:59 +0300

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,18 @@ class BasicAuth(Auth):
             return decode_value.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """Method to get user email and password from Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        else:
+            email, pwd = decoded_base64_authorization_header.split(":")
+            return email, pwd
