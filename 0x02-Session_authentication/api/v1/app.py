9f7468fc23006d71e2ad71dead148b2c231f9a09
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-10 04:30:36 +0300

    Task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,13 +33,13 @@ def filter_request():
         '/api/v1/forbidden/'
         ]
     if auth.require_auth(request.path, excluded_paths):
-            user = auth.current_user(request)
-            if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
-                abort(401)
-            if user is None:
-                abort(403)
-            request.current_user = user
+        user = auth.current_user(request)
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
+            abort(401)
+        if user is None:
+            abort(403)
+        request.current_user = user
 
 
 @app.errorhandler(404)
