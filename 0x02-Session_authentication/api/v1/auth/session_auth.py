37e2511839fdfb88c2db1120ef1bccf289f05ed6
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-11-10 04:38:43 +0300

    Task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+class to manage the session authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class to manage session authentication
+    """
